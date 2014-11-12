3b6f6389c6215340c84e849cce24d9def471be47
Yuxiang Wang <yw5aj@virginia.edu>
2014-11-11 22:22:48 -0500

    Cleaned gitignore

---
+++ .ipython/profile_default/startup/01_matplotlib_rc.py
@@ -0,0 +1,30 @@
+import matplotlib as mpl
+
+
+# Figure formatting
+mpl.rcParams['savefig.dpi'] = 300
+mpl.rcParams['figure.figsize'] = (3.27, 3.27)
+
+# Font setting
+mpl.rcParams['mathtext.default'] = 'regular'
+mpl.rcParams['font.family'] = ['sans-serif']
+mpl.rcParams['font.sans-serif'] = ['Arial']
+mpl.rcParams['font.size'] = 8
+mpl.rcParams['pdf.fonttype'] = 42
+mpl.rcParams['ps.fonttype'] = 42
+
+# Line properties
+mpl.rcParams['lines.linewidth'] = .75
+mpl.rcParams['lines.markersize'] = 3
+
+# Legends
+mpl.rcParams['legend.frameon'] = False
+mpl.rcParams['legend.fontsize'] = 8
+mpl.rcParams['legend.handlelength'] = 3
+
+# Subplot frame line
+mpl.rcParams['axes.linewidth'] = .5
+
+
+
+
